5d040e6c90ef97724382b1433989d2192dec6f3a
Chromadesk <yisraeleliyah@gmail.com>
2023-06-28 14:27:29 -0400

    he bites

--- src/ServerStorage/AiScripts/AiScriptVampire.server.lua
+++ src/ServerStorage/AiScripts/AiScriptVampire.server.lua
@@ -1,5 +1,6 @@
 local vampire = script.parent
 local humanoid = vampire.Humanoid
+local AttackHitbox = vampire:WaitForChild("AttackHitbox")
 
 --https://web.archive.org/web/20171120072253/http://wiki.roblox.com/index.php?title=Top_Down_Action/PiBot
 local function getTargetDistance(targetCharacter)
@@ -29,38 +30,65 @@ local function getClosestVisibleCharacter()
 end
 
 local stats = {}
-stats.FollowDistance = 50
-stats.StalkDistance = 20
-stats.ChaseSpeed = 25
-stats.LungeSpeed = 50
+stats.FOLLOW_DISTANCE = 50
+stats.STALK_DISTANCE = 20
+stats.CHASE_SPEED = 25
+stats.LUNGE_SPEED = 40
+stats.SLASH_DAMAGE = 50
+stats.BITE_DAMAGE = 999
 stats.State = "idle"
+stats.FollowPart = Instance.new("Part")
+    stats.FollowPart.Anchored = true
+    stats.FollowPart.CanCollide = false
+    stats.FollowPart.Transparency = 0.5
+    stats.FollowPart.Name = "FollowPart"
+
+local attacking = false
+AttackHitbox.touched:Connect(function(toucher)
+    local closestCharacter, _, closestAngle = getClosestVisibleCharacter()
+    if attacking or not toucher:IsDescendantOf(closestCharacter) then return end
+    attacking = true
+    
+    if closestAngle > 10 then
+        closestCharacter.Humanoid:TakeDamage(stats.BITE_DAMAGE)
+        print(stats.BITE_DAMAGE)
+        print(closestAngle)
+    else
+        closestCharacter.Humanoid:TakeDamage(stats.SLASH_DAMAGE)
+        print(stats.SLASH_DAMAGE)
+    end
+
+    wait(0.1)
+    attacking = false
+    AttackHitbox.CanTouch = false
+end)
 
 stats.EnterIdleState = function()
     stats.State = "idle"
     while stats.State == "idle" and wait(0.001)  do
         local closestCharacter, closestDistance = getClosestVisibleCharacter()
-        if closestDistance <= stats.FollowDistance then
+        if closestDistance <= stats.FOLLOW_DISTANCE then
             stats.EnterChaseState(closestCharacter)
         end
     end
 end
 
-local function orientNPC(target)
+local function orientNPC(position)
 	local HRP = vampire.HumanoidRootPart
-	local targetHRP = target.HumanoidRootPart
-    HRP.CFrame = CFrame.lookAt(HRP.CFrame.Position, Vector3.new(targetHRP.Position.X, HRP.CFrame.Position.Y, targetHRP.Position.Z))
+    HRP.CFrame = CFrame.new(HRP.CFrame.Position, Vector3.new(position.X, HRP.CFrame.Position.Y, position.Z))
 end
 
 stats.EnterChaseState = function(target)
     stats.State = "chase"
-    humanoid.WalkSpeed = stats.ChaseSpeed
+    humanoid.WalkSpeed = stats.CHASE_SPEED
     while stats.State == "chase" and wait(0.001) and target.Humanoid.Health > 0 do
-        orientNPC(target)
-        humanoid:MoveTo(target.HumanoidRootPart.Position)
+        orientNPC(target.HumanoidRootPart.Position)
+        stats.FollowPart.CFrame = target.HumanoidRootPart.CFrame
+        humanoid:MoveTo(stats.FollowPart.Position)
 
         local closestCharacter, closestDistance = getClosestVisibleCharacter()
         if not closestCharacter then break end
-        if closestDistance <= stats.StalkDistance then
+        if closestDistance <= stats.STALK_DISTANCE then
             stats.EnterStalkState(target)
         end
     end
@@ -69,15 +97,11 @@ end
 
 stats.EnterStalkState = function(target)
     stats.State = "stalk"
-    local maxDistance = stats.StalkDistance - math.random(1, 8)
+    humanoid.WalkSpeed = stats.CHASE_SPEED
+
+    local maxDistance = stats.STALK_DISTANCE - math.random(1, 8)
     local HRP = target.HumanoidRootPart
-    local followPart = Instance.new("Part")
-    followPart.Anchored = true
-    followPart.CanCollide = false
-    followPart.Transparency = 0.5
-    followPart.Name = "FollowPart"
-    followPart.CFrame = CFrame.new(HRP.Position)*CFrame.Angles(0,math.rad(0),0)*CFrame.new(0, 0, maxDistance)
-    followPart.Parent = workspace
+    stats.FollowPart.Parent = workspace
 
     local currentAngle = 0
     local secsToRedirect = math.random(1, 5)
@@ -87,13 +111,15 @@ stats.EnterStalkState = function(target)
     while stats.State == "stalk" and wait(0.001) do
         local closestCharacter, closestDistance, closestAngle = getClosestVisibleCharacter()
         if not closestCharacter then break end
-        if closestAngle > 10 then end
-        if closestDistance > stats.StalkDistance then
-            followPart:Destroy()
+        if closestAngle > 10 then
+            print(closestAngle)
+            stats.EnterLungeState(target)
+        end
+        if closestDistance > stats.STALK_DISTANCE then
             stats.EnterChaseState(target)
         end
         
-        followPart.CFrame = CFrame.new(HRP.Position)*CFrame.Angles(0,math.rad(currentAngle),0)*CFrame.new(0, 0, maxDistance)
+        stats.FollowPart.CFrame = CFrame.new(HRP.Position)*CFrame.Angles(0,math.rad(currentAngle),0)*CFrame.new(0, 0, maxDistance)
 
         if redirectCount >= secsToRedirect then
             rotationSpeed = 0 - rotationSpeed
@@ -102,8 +128,8 @@ stats.EnterStalkState = function(target)
         end
         currentAngle = currentAngle + rotationSpeed
 
-        orientNPC(target)
-        humanoid:MoveTo(followPart.Position)
+        orientNPC(target.HumanoidRootPart.Position)
+        humanoid:MoveTo(stats.FollowPart.Position)
         redirectCount = redirectCount + 0.05
     end
     stats.State = "idle"
@@ -111,7 +137,16 @@ end
 
 stats.EnterLungeState = function(target)
     stats.State = "lunge"
-    humanoid.WalkSpeed = stats.LungeSpeed
+    humanoid.WalkSpeed = stats.LUNGE_SPEED
+
+    orientNPC(target.HumanoidRootPart.Position)
+    stats.FollowPart.CFrame = vampire.HumanoidRootPart.CFrame * CFrame.new(0, 0, 0 - (stats.STALK_DISTANCE + 10))
+    humanoid:MoveTo(stats.FollowPart.Position)
+    AttackHitbox.CanTouch = true
+    wait(1)
+
+    wait(2.5)
+    stats.State = "idle"
 end
 
 stats.EnterIdleState()
