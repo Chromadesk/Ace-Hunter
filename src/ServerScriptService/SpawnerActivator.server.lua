d1e9d67bf322af08f5ca06e82045d4a95859d57d
Chromadesk <yisraeleliyah@gmail.com>
2023-07-26 09:22:09 -0400

    Spawner Reconfig

--- src/ServerScriptService/SpawnerActivator.server.lua
+++ src/ServerScriptService/SpawnerActivator.server.lua
@@ -2,15 +2,16 @@ local AiResources = game:GetService("ServerStorage").AiResources
 local nextId = 0
 
 for _,spawn in pairs(workspace:GetChildren()) do
-    if spawn.Name ~= "Spawner" then return end
-    spawn.Transparency = 1
+    if spawn.Name == "Spawner" then
+        spawn.Transparency = 1
 
-    local id = Instance.new("NumberValue")
-    id.Name = "Id"
-    id.Value = nextId
-    nextId = nextId + 1
-    id.Parent = spawn
+        local id = Instance.new("NumberValue") --Create an ID for the spawner that is given to all of its spawned NPCs
+        id.Name = "Id"
+        id.Value = nextId
+        nextId = nextId + 1
+        id.Parent = spawn
 
-    local spawnerScript = AiResources.Spawner.SpawnerScript:Clone()
-    spawnerScript.Parent = spawn
+        local spawnerScript = AiResources.Spawner.SpawnerScript:Clone() --Give spawner the SpawnerScript
+        spawnerScript.Parent = spawn
+    end
 end
--- src/ServerStorage/AiResources/Spawner/SpawnerScript.server.lua
+++ src/ServerStorage/AiResources/Spawner/SpawnerScript.server.lua
@@ -1,15 +1,18 @@
 local ServerStorage = game:GetService("ServerStorage")
 local Spawner = script.Parent
-local NPC = ServerStorage.AiEnemies[Spawner.NPCName]
+local NPC = ServerStorage.AiEnemies[Spawner.SpawnTarget.Value]
 
 function activateSpawn()
     if not Spawner.IsActive.Value then return end
-    local copyNPC = NPC:Clone()
+
+    local copyNPC = NPC:Clone() --Grab the SpawnTarget NPC and give them the Spawner's ID
     local copyId = Spawner.Id:Clone()
     copyId.Parent = copyNPC
-    copyNPC.Parent = workspace.NPCs
+    copyNPC.Parent = workspace.NPCs --Spawn in the now ID'd SpawnTarget NPC
     copyNPC:MoveTo(Spawner.Position)
 
-    wait(spawn.SpawnRate.Value)
+    wait(Spawner.SpawnRate.Value) --Wait however many seconds SpawnRate says until trying to spawn an NPC again
     activateSpawn()
 end
+
+activateSpawn()
